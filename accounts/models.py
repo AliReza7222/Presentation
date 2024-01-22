3a23c06652e7d7d25431b97cda83eb0726764824
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-22 15:10:56 +0330

    change type id User and create model Profile for user

--- accounts/models.py
+++ accounts/models.py
@@ -1,14 +1,23 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=50)
     created_at = models.DateTimeField(auto_now=True, editable=False)
-    updated_at = models.DateTimeField(auto_now=True, editable=True)
+    updated_at = models.DateTimeField(auto_now=True)
     activation_code = models.CharField(max_length=16)
 
     def __str__(self):
         return self.username
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    updated_at = models.DateTimeField(auto_now=True)
 
