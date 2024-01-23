64bf7a213380989d6af9333d8396033dc5715446
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-23 10:50:45 +0330

    updated installed app and manage files media

--- config/settings.py
+++ config/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+    'presentations.apps.PresentationsConfig',
+    'Slide.apps.SlideConfig'
 
 ]
 
@@ -121,6 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Manage Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
