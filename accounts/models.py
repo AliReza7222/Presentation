2b911c93877c6b2a42a1dead88714facb2e4c117
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-23 10:49:56 +0330

    update filed created_at and updated_at

--- accounts/models.py
+++ accounts/models.py
@@ -7,7 +7,7 @@ class User(AbstractUser):
     id = models.UUIDField(default=uuid.uuid4, editable=False, primary_key=True)
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now=True, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True)
     activation_code = models.CharField(max_length=16)
 
@@ -19,5 +19,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-    updated_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField()
 
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
