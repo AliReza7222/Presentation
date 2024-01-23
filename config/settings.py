e928228466f881a1c8bd8479161fe3d81327632b
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-23 11:23:45 +0330

    update project

--- config/settings.py
+++ config/settings.py
@@ -9,18 +9,23 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+import os
 
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+# env var config
+load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-oeg=u)o72v61%9s2r%bl*p6h257m$k_gis))bq)tu^+f@#6pwm'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # My Custom User
 AUTH_USER_MODEL = 'accounts.User'
