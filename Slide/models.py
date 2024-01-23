fb9684cd211f1dc60712454572a5cc3d72e9000e
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-23 10:51:50 +0330

    create app Slide

---
+++ Slide/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+from presentations.models import Presentation
+
+
+class Slide(models.Model):
+    section_link = models.CharField(max_length=255)
+    section_id = models.CharField(max_length=150)
+    content = models.TextField()
+    presentation_id = models.ForeignKey(Presentation, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True)
