86dbb082bd132863e3498612bb6f1cb12471f5dc
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-31 00:48:06 +0330

    endpoint update slide complete

--- Slide/api/v1/urls.py
+++ Slide/api/v1/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from Slide.api.v1.views import CreateSlideView
+from Slide.api.v1.views import *
 
 
 app_name = "Slide"
 urlpatterns = [
-    path("create/", CreateSlideView.as_view(), name="create_slide")
+    path("create/", CreateSlideView.as_view(), name="create_slide"),
+    path("update/<int:pk>/", UpdateSlideView.as_view(), name='update_slide')
 ]
