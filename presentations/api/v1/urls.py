9d7daa572648190d90156549115f456024a740bd
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-30 21:17:32 +0330

    endpoint update presentation complete

--- presentations/api/v1/urls.py
+++ presentations/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from presentations.api.v1.views import CreatePresentationView, CreateTagView
+from presentations.api.v1.views import *
 
 app_name = "presentations"
 
 urlpatterns = [
     path("create/", CreatePresentationView.as_view(), name='create_presentation'),
-    path("create/tag/", CreateTagView.as_view(), name="create_tag")
+    path("create/tag/", CreateTagView.as_view(), name="create_tag"),
+    path("update/<int:pk>/", UpdatePresentationView.as_view(), name='update_presentation')
 ]
