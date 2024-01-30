aab4c25853b1bb39458bbdda6ce9d080569adf14
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-30 19:16:17 +0330

    endpoint update profile complete

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from accounts.api.v1.views import SignUpUserView, SignInUserView
+from accounts.api.v1.views import *
 
 
 app_name = 'accounts'
 urlpatterns = [
     path('signup/', SignUpUserView.as_view(), name="sign_up"),
-    path('signin/', SignInUserView.as_view(), name='sign_in')
+    path('signin/', SignInUserView.as_view(), name='sign_in'),
+    path('update/profile/<int:pk>/', UpdateProfileUserView.as_view(), name='update_profile'),
 ]
--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -1,7 +1,9 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
 
 from accounts.api.v1.serializers import *
+from accounts.models import Profile
 
 
 class SignUpUserView(CreateAPIView):
@@ -10,3 +12,9 @@ class SignUpUserView(CreateAPIView):
 
 class SignInUserView(TokenObtainPairView):
     serializer_class = SignInSerializer
+
+
+class UpdateProfileUserView(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSrializer
+    queryset = Profile.objects.all()
