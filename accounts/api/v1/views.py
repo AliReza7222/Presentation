e168b3dd6b8b6690e4720f148bf1f3aaaf8804ca
AliReza7222 <ali.r.salehi99@gmail.com>
2024-01-30 19:38:44 +0330

    update view api v1

--- accounts/api/v1/views.py
+++ accounts/api/v1/views.py
@@ -1,6 +1,7 @@
 from rest_framework.generics import CreateAPIView, UpdateAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from accounts.api.v1.serializers import *
 from accounts.models import Profile
@@ -18,3 +19,19 @@ class UpdateProfileUserView(UpdateAPIView):
     permission_classes = [IsAuthenticated]
     serializer_class = ProfileSrializer
     queryset = Profile.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        data = request.data.copy()
+        data['user'] = request.user.id
+        serializer = self.get_serializer(instance, data=data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
