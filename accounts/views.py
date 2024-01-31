ca27be9e701615b4cb476fa04cb7b2bbcf7b18b0
AliReza7222 <ali.r.salehi99@gmail.com>
2024-02-01 03:23:14 +0330

    change directory api apps to apps

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, UpdateAPIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
-# Create your views here.
+from accounts.api.v1.serializers import *
+from accounts.models import Profile
+
+
+class SignUpUserView(CreateAPIView):
+    serializer_class = UserSerializer
+
+
+class SignInUserView(TokenObtainPairView):
+    serializer_class = SignInSerializer
+
+
+class UpdateProfileUserView(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSrializer
+    queryset = Profile.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
--- presentations/views.py
+++ presentations/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.generics import CreateAPIView, UpdateAPIView, DestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from presentations.api.v1.serializers import *
+from presentations.models import Presentation, Tag
+
+
+class CreatePresentationView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PresentationSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['user'] = request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class CreateTagView(CreateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TagSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data.copy()
+        data['user'] = request.user.id
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class UpdatePresentationView(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = PresentationSerializer
+    queryset = Presentation.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+class UpdateTagView(UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = TagSerializer
+    queryset = Tag.objects.all()
+
+    def update(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
+
+        return Response(serializer.data)
+
+
+class DeletePresentationView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Presentation.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(data={'message': 'The presentation was successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
+
+
+class DeleteTagView(DestroyAPIView):
+    permission_classes = [IsAuthenticated]
+    queryset = Tag.objects.all()
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        self.perform_destroy(instance)
+        return Response(data={'message': 'The tag was successfully deleted'}, status=status.HTTP_204_NO_CONTENT)
