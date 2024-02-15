a6b24a8d36c716aec09f5b3260888ba5aaadc914
AliReza7222 <ali.r.salehi99@gmail.com>
2024-02-15 15:01:55 +0330

    fix a problem to get data sections with slug parametr

--- digikala/views.py
+++ digikala/views.py
@@ -2,6 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 
 from utils.data_digikala import DigiKalaData
 
@@ -17,9 +19,12 @@ class GetChaptersView(APIView):
 
 class GetSectionView(APIView):
     permission_classes = (IsAuthenticated, )
+    slug = openapi.Parameter('slug', openapi.IN_QUERY,
+                             type=openapi.TYPE_STRING)
 
+    @swagger_auto_schema(manual_parameters=[slug])
     def get(self, request, **kwargs):
-        slug = kwargs.get('slug') or 'None'
+        slug = request.query_params.get('slug') or 'None'
         data = DigiKalaData.get_section(slug)
         status_request = data.get('status', status.HTTP_200_OK)
         return Response(data, status_request)
