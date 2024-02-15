ec28e128560b217e2a1ed6370bd67b0525480fe2
AliReza7222 <ali.r.salehi99@gmail.com>
2024-02-15 17:45:16 +0330

    complete slugify field slug in presentation .

--- presentation/serializers.py
+++ presentation/serializers.py
@@ -1,4 +1,5 @@
 from django.db import transaction
+from django.utils.text import slugify
 from rest_framework import serializers
 
 from .models import Presentation
@@ -12,6 +13,12 @@ class PresentationSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'user': {'read_only': True}
         }
+
+    def validate_slug(self, slug):
+        slug = slugify(slug)
+        if not slug:
+            raise serializers.ValidationError("Please enter a valid slug !")
+        return slug
 
     def create(self, validated_data):
         user = self.context['request'].user
